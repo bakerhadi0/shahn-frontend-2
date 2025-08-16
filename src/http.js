7f60eecfefa5ff0d10ef0f4145d6cbe7f3cf8938
bakerhadi0 <bakerhadi0@users.noreply.github.com>
2025-08-16 04:40:14 +0300

    Frontend: customers use auth token; add login; wire API

---
+++ src/http.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE || "https://shahn-server.onrender.com",
+});
+api.interceptors.request.use((config) => {
+  const t = localStorage.getItem("token");
+  if (t) config.headers.Authorization = `Bearer ${t}`;
+  return config;
+});
+export default api;
